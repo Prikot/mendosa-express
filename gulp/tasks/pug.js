93eea0c8c39ad7b4bc6ef80802d1f45a70233855
Prikot <a.prikotov@gmail.com>
2018-03-09 14:53:06 +0300

    nodemon added

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,18 +1,36 @@
 'use strict';
-
 module.exports = function() {
   $.gulp.task('pug', function() {
-    var locals = require('./content.json');
-    return $.gulp.src('./views/pages/*.pug')
-      .pipe($.gp.pug({
-        locals: locals,
-        pretty: true}))
-      .on('error', $.gp.notify.onError(function(error) {
-        return {
-          title: 'Pug',
-          message:  error.message
-        }
-       }))
-      .pipe($.gulp.dest($.config.root));
+    return $.gulp.src('./source/template/pages/*.pug')
+        .pipe($.gp.pug({ pretty: true }))
+        .on('error', $.gp.notify.onError(function(error) {
+          return {
+            title: 'Pug',
+            message:  error.message
+          }
+        }))
+        .pipe($.gulp.dest($.config.root));
   });
 };
+
+
+
+// module.exports = function() {
+//   $.gulp.task('pug', function() {
+//     return $.gulp.src('./views/pages/*.pug')
+//       .pipe($.gp.pug({
+//         // var locals = require('./content.json');/
+//         locals: locals,
+//         pretty: true}))
+//       .on('error', $.gp.notify.onError(function(error) {
+//         return {
+//           title: 'Pug',
+//           message:  error.message
+//         }
+//        }))
+//       .pipe($.gulp.dest($.config.root));
+//   });
+// };
+
+// var locals = require('./content.json');
+
