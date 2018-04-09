a03cc7ffd01a2266152d76490807ed5d8d265367
Prikot <a.prikotov@gmail.com>
2018-04-09 14:59:15 +0300

    urls changes

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/index', { title: 'Главная' });
 });
 
 router.get('/index', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/index', { title: 'Главная' });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('pages/about', { title: 'О нас' });
 });
 
-router.get('/studio', function(req, res, next) {
-  res.render('pages/studio', { title: 'О нас' });
+router.get('/tissue', function(req, res, next) {
+  res.render('pages/tissue', { title: 'Ткани' });
 });
+
+router.get('/studio', function(req, res, next) {
+  res.render('pages/studio', { title: 'Ателье' });
+});
+
+router.get('/showroom', function(req, res, next) {
+  res.render('pages/showroom', { title: 'Шоурум' });
+});
+
+router.get('/gallery', function(req, res, next) {
+  res.render('pages/gallery', { title: 'О нас' });
+});
+
 
 module.exports = router;
